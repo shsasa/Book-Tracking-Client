0d365841b4311246fc951cd684ecf434fc64b67c
Sayed husain <shsasa@gmail.com>
2025-06-03 14:24:12 +0300

    add usercard

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,40 @@
+import UserCard from '../components/UserCard';
+import { useState, useEffect } from 'react';
+import { getAllUsers } from '../services/User';
 const Admin = () => {
+
+  const [users, setUsers] = useState([]);
+
+
+
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const userList = await getAllUsers();
+        setUsers(userList);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+
   return (
     <div className="admin-page">
       <h1>Admin Page</h1>
       <p>This is the admin page where you can manage the application.</p>
-      {/* Add more admin functionalities here */}
+      {users.length > 0 ? (
+        <div className="user-list">
+          {users.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </div>
+      ) : (
+        <p>No users found.</p>
+      )}
     </div>
   );
 }
